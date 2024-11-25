9fa34d8ee1dab43a6daf27984c89816e83f519b3
ranvie <neodyte@gmail.com>
2024-11-25 13:39:47 +0700

    Sus

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,25 +4,28 @@ import ProcessedImages from './images'; // Adjust the path if necessary
 import Graph from './graph';
 import Edit from './edit';
 
-import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation'
 
-
-export default function Page() {
+function ByUser() {
   const searchParams = useSearchParams()
-  const user = searchParams.get('user')
-
+  const user = searchParams.get('user');
   if (!user) {
     return <h3>Error: No user provided in the URL</h3>;
   }
-
-  return (
-  <div>
-    <Suspense>
-    <h3>Username: {user}</h3>
+  return(
+    <div>
     <Edit username={user}/>
     <Graph username={user}/>
     <ProcessedImages username={user}/>
+    </div>
+  )     
+}
+
+export default function page() {
+  return(
+    <Suspense>
+      <ByUser/>
     </Suspense>
-  </div>)
+  )
 }
